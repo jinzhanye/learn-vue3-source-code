4d429d6142c05c5fbabebbcf9525ce09c08c1c4b
yejinzhan <jinzhan.ye@foxmail.com>
2022-12-10 00:24:20 +0800

    course-1

---
+++ README.md
@@ -0,0 +1,5 @@
+# learn-vue3-source-code
+
+https://www.vuemastery.com/courses/vue-3-reactivity/vue3-reactivity
+
+https://www.bilibili.com/video/BV1SZ4y1x7a9
---
+++ src/1.js
@@ -0,0 +1,44 @@
+const targetMap = new WeakMap()
+
+function track(target, key) {
+  let depsMap = targetMap.get(target)
+
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+
+  dep.add(effect)
+}
+
+function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    return
+  }
+
+  let dep = depsMap.get(key)
+  if (dep) {
+    dep.forEach(effect => {
+      effect()
+    })
+  }
+}
+
+let product = { price: 5, quantity: 2 }
+let total = 0
+let effect = () => {
+  total = product.price * product.quantity
+}
+
+track(product, 'quantity')
+effect()
+console.log(total)// 10
+
+product.quantity = 3
+trigger(product, 'quantity')
+console.log(total)// 15
